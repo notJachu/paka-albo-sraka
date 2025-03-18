5807731d2209d73f27c78c835d82fab0fec3ec2e
notJachu <jasiustasiu50@interia.pl>
2025-03-18 20:57:36 +0100

    saving results to json

--- live/index.js
+++ live/index.js
@@ -3,6 +3,11 @@ const cookieParser = require('cookie-parser');
 const app = express()
 const port = 80
 
+const save_path ='./results.json';
+const fs = require('fs');
+const { type } = require('os');
+var save_file = require(save_path);
+
 app.get('/', (req, res) => {
   //res.send("Hello World");
     res.cookie('userID', 2137);
@@ -19,6 +24,27 @@ app.post('/', express.urlencoded({ extended: true }), (req, res) => {
   //console.log(req);
   //res.sendFile(__dirname + '/front/response.html');
   res.send(formData);
+
+  // parse data
+  console.log(save_file);
+  console.log(typeof save_file.place);
+  console.log(typeof save_file.place[0]);
+  if (save_file.timestamp) {
+    const lastTimestamp = new Date(save_file.timestamp);
+    const currentTimestamp = new Date();
+    const timeDifference = currentTimestamp - lastTimestamp; // Difference in milliseconds
+    console.log(`Time since last timestamp: ${timeDifference} ms`);
+  } else {
+    console.log('No previous timestamp found.');
+  }
+  save_file.timestamp = new Date().toString();
+
+  // Save data
+  fs.writeFile
+  (save_path, JSON.stringify(save_file), (err) => {
+    if (err) throw err;
+    console.log('Data has been saved!');
+  });
 });
 
 app.listen(port, () => {
