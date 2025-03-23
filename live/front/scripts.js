b44d6552e7f7dba3968bba5ce1e0b11b0e235b6f
notJachu <jasiustasiu50@interia.pl>
2025-03-23 02:14:28 +0100

    implemented basic form post handling

--- live/front/scripts.js
+++ live/front/scripts.js
@@ -1,31 +1,21 @@
-const ctx = document.getElementById('myChart');
 
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [
-            'PAKA',
-            'SRAKA'
-        ],
-        datasets: [{
-            label: 'Votes',
-            data: [55, 32],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
+window.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("forma");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    for (let [key, value] of formData.entries()) {
+      console.log(key, value);
     }
+    console.log("Sending data...");
+
+    fetch("/api", {
+      method: "POST",
+      body: formData,
+    })
+      .then((data) => {
+        console.log(data);
+      });
+  });
 });
--- live/index.js
+++ live/index.js
@@ -6,8 +6,16 @@ app.get('/', (req, res) => {
   //res.send("Hello World");
     res.sendFile(__dirname + '/front/index.html');
 })
+
+app.post('/', express.urlencoded({ extended: true }), (req, res) => {
+  const formData = req.body;
+  console.log('Form Data:', formData);
+  res.redirect('/');
+});
+
 app.use(express.static('front'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
