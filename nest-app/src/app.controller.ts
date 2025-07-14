8fe4980df495b2e858acfd32db8f46eca0513815
notJachu <jasiustasiu50@interia.pl>
2025-07-15 01:05:06 +0200

    installed hbs

--- nest-app/src/app.controller.ts
+++ nest-app/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, Render } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  root(@Res() res: Response) {
+    return res.render(
+      this.appService.getViewName(),
+      { message: 'Hello world!' },
+    );
   }
 }
