973e72f836e4429a8a8f723ff1018a3116e9a262
notJachu <jasiustasiu50@interia.pl>
2026-01-18 00:26:01 +0100

    event service

---
+++ paka-java/src/main/java/jachu/pg/pakajava/services/EventService.java
@@ -0,0 +1,19 @@
+package jachu.pg.pakajava.services;
+
+
+import jachu.pg.pakajava.repositories.EventRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    @Autowired
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+}
