7a58ce55a9317c79eb8ff35a8182679c89fa65e1
notJachu <jasiustasiu50@interia.pl>
2026-01-18 00:11:28 +0100

    init java app

---
+++ paka-java/src/main/java/jachu/pg/pakajava/PakaJavaApplication.java
@@ -0,0 +1,13 @@
+package jachu.pg.pakajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PakaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PakaJavaApplication.class, args);
+    }
+
+}
---
+++ paka-java/src/test/java/jachu/pg/pakajava/PakaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package jachu.pg.pakajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PakaJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
