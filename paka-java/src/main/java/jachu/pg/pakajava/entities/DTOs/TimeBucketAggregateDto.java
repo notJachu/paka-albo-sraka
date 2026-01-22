3d2c9765c60beec84a3c0c0a4cdd572b4333c1e8
notJachu <jasiustasiu50@interia.pl>
2026-01-22 22:55:38 +0100

    Add lombok to DTOs

--- paka-java/src/main/java/jachu/pg/pakajava/entities/DTOs/TimeBucketAggregateDto.java
+++ paka-java/src/main/java/jachu/pg/pakajava/entities/DTOs/TimeBucketAggregateDto.java
@@ -1,5 +1,14 @@
 package jachu.pg.pakajava.entities.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TimeBucketAggregateDto {
     int votes_paka;
     int votes_sraka;
--- paka-java/src/main/java/jachu/pg/pakajava/entities/DTOs/TimeBucketCollectionDto.java
+++ paka-java/src/main/java/jachu/pg/pakajava/entities/DTOs/TimeBucketCollectionDto.java
@@ -1,5 +1,14 @@
 package jachu.pg.pakajava.entities.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TimeBucketCollectionDto {
     String date;
     int votes_paka;
