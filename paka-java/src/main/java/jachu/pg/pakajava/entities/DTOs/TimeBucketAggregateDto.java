8ac97fa4cfb7354a4486b73254f3dd35ab9e19e2
notJachu <jasiustasiu50@interia.pl>
2026-01-22 22:43:09 +0100

    time bucket DTOs

---
+++ paka-java/src/main/java/jachu/pg/pakajava/entities/DTOs/TimeBucketAggregateDto.java
@@ -0,0 +1,6 @@
+package jachu.pg.pakajava.entities.DTOs;
+
+public class TimeBucketAggregateDto {
+    int votes_paka;
+    int votes_sraka;
+}
---
+++ paka-java/src/main/java/jachu/pg/pakajava/entities/DTOs/TimeBucketCollectionDto.java
@@ -0,0 +1,7 @@
+package jachu.pg.pakajava.entities.DTOs;
+
+public class TimeBucketCollectionDto {
+    String date;
+    int votes_paka;
+    int votes_sraka;
+}
