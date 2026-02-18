43a321955a1da51fd9c8fe0d65b562a6bae631d8
notJachu <jasiustasiu50@interia.pl>
2026-02-18 18:32:10 +0100

    use signals

--- paka-ui/src/app/vote-form/vote-form.ts
+++ paka-ui/src/app/vote-form/vote-form.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {RecaptchaModule, RecaptchaFormsModule} from 'ng-recaptcha-2';
 import {MatButton} from '@angular/material/button';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
 import {EventReadDto} from '../EventReadDto';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-vote-form',
@@ -12,7 +13,7 @@ import {EventReadDto} from '../EventReadDto';
     FormsModule,
     RecaptchaModule,
     RecaptchaFormsModule,
-    MatButton
+    MatButton,
   ],
   templateUrl: './vote-form.html',
   styleUrl: './vote-form.css',
@@ -24,30 +25,30 @@ export class VoteForm {
   private eventUrl: string = '';
   protected captchaToken: string = '';
 
-  protected eventData: EventReadDto | null = null;
-
   private http = inject(HttpClient);
   private route = inject(ActivatedRoute);
 
+  private eventId = this.route.snapshot.paramMap.get('eventId') || 'default';
+
+  eventData = toSignal(
+    this.http.get<EventReadDto>(`http://localhost:8080/api/events/${this.eventId}`)
+  );
+
   constructor() {
-    const evetId = this.route.snapshot.paramMap.get('eventId');
-    if (evetId) {
-      this.eventUrl = `http://localhost:8080/api/events/${evetId}`;
-      this.fetchEvent();
-    } else {
-      // Default event on the landing page
-      this.eventUrl = 'http://localhost:8080/api/events/default';
-      this.fetchEvent();
-    }
   }
 
   onSubmit(value: any) {
     console.log('Form submitted');
+    const event = this.eventData();
+    if (!event) {
+      console.error('Event data is not available');
+      return;
+    }
     const finalData = {
       captcha: this.captchaToken,
       vote: value
     };
-    this.http.post('http://localhost:8080/api/events/'+ this.eventData?.eventId +'/vote?vote=' + value, finalData).subscribe(response => {
+    this.http.post('http://localhost:8080/api/events/'+ event.eventId + '/vote?vote=' + value, finalData).subscribe(response => {
       console.log('Vote submitted successfully:', response);
       this.hasVoted = true;
     }, error => {
@@ -59,16 +60,6 @@ export class VoteForm {
     console.log('Captcha value:', event);
     this.isCaptchaCompleted = true;
     this.captchaToken = event;
-  }
-
-  fetchEvent() {
-    console.log('Fetching event data...');
-    this.http.get<EventReadDto>(this.eventUrl).subscribe(data => {
-      this.eventData = data;
-      console.log('Event data fetched:', this.eventData);
-    }, error => {
-      console.error('Error fetching event data:', error);
-    });
   }
 
 }
